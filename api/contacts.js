de5d74862c2c073e32c3b5b8e7d264d35283a9eb
Mykola Pankov <pankoff97@gmail.com>
2023-04-21 20:52:27 +0300

    21/2 jw-pss

--- api/contacts.js
+++ api/contacts.js
@@ -4,7 +4,7 @@ const contactController = require('../controllers/contacts');
 const { auth } = require('../middlewares/auth');
 
 router.get('/', auth, contactController.getAll);
-router.get('/:contactId', auth, contactController.getById);
+5('/:contactId', auth, contactController.getById);
 router.post('', auth, contactController.addContact);
 router.put('/:contactId', auth, contactController.updateContact);
 router.patch('/:contactId/favorite', auth, contactController.setFavorite);
