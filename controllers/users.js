106ad19689c0d7a6d4e25a46c1aac7f103e36467
Mykola Pankov <pankoff97@gmail.com>
2023-04-30 09:58:40 +0300

    1

--- controllers/users.js
+++ controllers/users.js
@@ -1,17 +1,21 @@
-const service = require('../service/users');
 const { userValidator } = require('./../utils/validators/validator');
+const service = require('../service/users');
 const jwt = require('jsonwebtoken');
 const User = require('../service/schemas/user');
 require('dotenv').config();
-
+const gravatar = require('gravatar');
+const fs = require('fs/promises');
+const path = require('path');
+const Jimp = require('jimp');
+const { imageStore } = require('../middleware/upload');
 const secret = process.env.SECRET;
 
 const register = async (req, res, next) => {
   const { error } = userValidator(req.body);
   if (error) return res.status(400).json({ message: error.details[0].message });
-
   const { email, password, subscription } = req.body;
   const user = await service.getUser({ email });
+
   if (user) {
     return res.status(409).json({
       status: 'error',
@@ -21,18 +25,20 @@ const register = async (req, res, next) => {
     });
   }
   try {
-    const newUser = new User({
-      email,
-      password,
-      subscription,
+    const avatarURL = gravatar.url(email, {
+      s: '200',
+      r: 'pg',
+      d: 'mm',
     });
+
+    const newUser = new User({ email, password, subscription, avatarURL });
     newUser.setPassword(password);
     await newUser.save();
     res.status(201).json({
       status: 'success',
       code: 201,
       data: {
-        message: 'Successful registration',
+        message: 'Registration successful',
       },
     });
   } catch (error) {
@@ -52,22 +58,27 @@ const login = async (req, res, next) => {
       status: 'error',
       code: 401,
       message: 'Incorrect email or password',
-      data: 'Unothorized',
+      data: 'Unauthorized',
     });
   }
+
   const payload = {
     id: user.id,
-    email: user.mail,
+    email: user.email,
   };
 
   const token = jwt.sign(payload, secret, { expiresIn: '1h' });
   user.setToken(token);
   await user.save();
-  res.json({
+  res.status(200).json({
     status: 'success',
     code: 200,
     data: {
       token,
+      user: {
+        email: user.email,
+        subscription: user.subscription,
+      },
     },
   });
 };
@@ -123,10 +134,100 @@ const getUsers = async (req, res, next) => {
   });
 };
 
+const updateSubscription = async (req, res, next) => {
+  try {
+    const { error } = userValidator(req.body);
+    if (error)
+      return res.status(400).json({ message: error.details[0].message });
+    const { subscription } = req.body;
+    const { userId } = req.params;
+
+    if (!subscription) {
+      res.status(400).json({ message: 'missing field subscription' });
+    }
+    const user = await service.updateUserSubscription(userId, subscription);
+
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: 'Not found' });
+    }
+  } catch (error) {
+    console.error(error.message);
+    next(error);
+  }
+};
+
+const updateAvatar = async (req, res, next) => {
+  if (!req.file) {
+    return res.status(400).json({ message: 'There is no file' });
+  }
+  const { description } = req.body;
+  const { path: temporaryName } = req.file;
+  const fileName = path.join(imageStore, req.file.filename);
+
+  const newUser = await service.updateUserAvatar(req.body.id, fileName);
+  try {
+    await fs.rename(temporaryName, fileName);
+  } catch (err) {
+    await fs.unlink(temporaryName);
+    return next(err);
+  }
+
+  const isValid = await isCorrectResizedImage(fileName);
+  if (!isValid) {
+    await fs.unlink(fileName);
+    return res
+      .status(400)
+      .json({ message: 'File is not a photo or problem during resizing' });
+  }
+
+  res.json({
+    description,
+    fileName,
+    avatarURL: newUser.avatarURL,
+    message: 'File uploaded correctly',
+    status: 200,
+  });
+};
+
+const isCorrectResizedImage = async (imagePath) =>
+  new Promise((resolve) => {
+    try {
+      Jimp.read(imagePath, (error, image) => {
+        if (error) {
+          resolve(false);
+        } else {
+          image.resize(250, 250).write(imagePath);
+          resolve(true);
+        }
+      });
+    } catch (error) {
+      resolve(false);
+    }
+  });
+
+const deleteUserByMail = async (req, res) => {
+  try {
+    const email = req.query.email;
+    const userToRemove = await service.deleteUser(email);
+    if (!userToRemove) {
+      return res.status(404).json({ message: 'Not found user' });
+    } else {
+      res.status(200).json({ message: 'User deleted from data base' });
+    }
+  } catch (error) {
+    console.log(`Error: ${error.message}`.red);
+  }
+};
+
 module.exports = {
   register,
   login,
   logout,
   current,
   getUsers,
+  updateSubscription,
+  updateAvatar,
+  deleteUserByMail,
 };
